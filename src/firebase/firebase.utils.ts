db8b48f19d57482e8d77ef13df835a568ba46750
Spencer Williams <spencer@aninternetpresence.net>
2021-03-01 22:48:19 -0600

    Use config.ts
      * Use latest yarn
      * Use {prompt: 'select_account'} for Google login so that user can a Google account

--- src/firebase/firebase.utils.ts
+++ src/firebase/firebase.utils.ts
@@ -1,16 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-
-const config = {
-    apiKey: "AIzaSyBWHeVIbwQ4ifcSlGKn6kau41NJFycLzU8",
-    authDomain: "crwn-db-b7030.firebaseapp.com",
-    databaseURL: "https://crwn-db-b7030.firebaseio.com",
-    projectId: "crwn-db-b7030",
-    storageBucket: "crwn-db-b7030.appspot.com",
-    messagingSenderId: "26008414346",
-    appId: "1:26008414346:web:1ae3327f8da0b538ede573"
-};
+import config from '../config';
 
 export const createUserProfileDocument = async (userAuth: firebase.UserInfo | null, additionalData: {}) => {
     if(!userAuth) return;
@@ -35,12 +26,13 @@ export const createUserProfileDocument = async (userAuth: firebase.UserInfo | nu
     return userRef;
 }
 
-firebase.initializeApp(config);
+firebase.initializeApp(config.firebase);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
